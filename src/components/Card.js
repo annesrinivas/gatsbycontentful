37ad850218f2725984dc9f790cd860d52f703d0f
Sri <annesrinivas@gmail.com>
2020-04-17 20:45:24 -0400

    Using emotion js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React from 'react'
-/* import styled from '@emotion/styled' */
-import styled from 'styled-components'
+import styled from '@emotion/styled' 
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
